cec3839f8f6e0d0386cf87cfa26bc728371f3e14
Adophilus <uchenna19of@gmail.com>
2022-10-09 15:37:05 +0100

    updated footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,18 +4,44 @@ const Footer: React.FC = () => {
   return (
     <article
       style={{
-        textAlign: 'center'
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column'
       }}
     >
-      Made with{' '}
-      <i style={{ color: '#F93822' }} className="bi bi-heart-fill"></i> by{' '}
-      <a
-        rel="noref noopener"
-        target="_blank"
-        href="https://github.com/Adophilus"
+      <span>
+        Made with{' '}
+        <i style={{ color: '#F93822' }} className="bi bi-heart-fill"></i> by{' '}
+        <a
+          rel="noreferrer noopener"
+          target="_blank"
+          href="https://github.com/Adophilus"
+        >
+          Uchenna Ofoma
+        </a>
+      </span>
+      <span
+        style={{
+          display: 'flex',
+          columnGap: 'calc(var(--block-spacing-vertical) / 2)'
+        }}
       >
-        Uchenna Ofoma
-      </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://twitter.com/adophilus"
+        >
+          <i className="bi bi-twitter"></i>
+        </a>
+        <a
+          style={{ color: 'black' }}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/Adophilus"
+        >
+          <i className="bi bi-github"></i>
+        </a>
+      </span>
     </article>
   )
 }
