b214d7dcdef79c69e33df31b2152e32856bf7e96
Adophilus <uchenna19of@gmail.com>
2022-10-10 11:54:18 +0100

    added banner query

--- src/services/Backend.ts
+++ src/services/Backend.ts
@@ -6,15 +6,38 @@ import {
   IProduct
 } from '../types/Collections'
 
-interface IProductsQueryParams {
+interface IPocketBaseRecordQueryParams {
   page?: number
   perPage?: number
   filter?: string
   sort?: string
 }
+
+type IProductsQueryParams = IPocketBaseRecordQueryParams
+type IBannersQueryParams = IPocketBaseRecordQueryParams
+
 interface IProductQueryParams {
   id?: string
   sku?: number
+}
+
+const prepareQuery = (
+  params: IPocketBaseRecordQueryParams
+): URLSearchParams => {
+  const _query = new URLSearchParams()
+  if (params.page != null) {
+    _query.append('page', params.page.toString())
+  }
+  if (params.perPage != null) {
+    _query.append('perPage', params.perPage.toString())
+  }
+  if (params.filter != null) {
+    _query.append('filter', params.filter)
+  }
+  if (params.sort != null) {
+    _query.append('sort', params.sort)
+  }
+  return _query
 }
 
 export const backendApi = createApi({
@@ -56,21 +79,20 @@ export const backendApi = createApi({
         return response.message
       }
     }),
+    getBanners: builder.query<IListResult, IBannersQueryParams>({
+      query(params: IBannersQueryParams) {
+        const query = prepareQuery(params)
+        return {
+          url: `banners?${query.toString()}`
+        }
+      },
+      transformResponse(response: { message: IListResult }): IListResult {
+        return response.message
+      }
+    }),
     getProducts: builder.query<IListResult, IProductsQueryParams>({
       query(params: IProductsQueryParams) {
-        const _query = new URLSearchParams()
-        if (params.page != null) {
-          _query.append('page', params.page.toString())
-        }
-        if (params.perPage != null) {
-          _query.append('perPage', params.perPage.toString())
-        }
-        if (params.filter != null) {
-          _query.append('filter', params.filter)
-        }
-        if (params.sort != null) {
-          _query.append('sort', params.sort)
-        }
+        const _query = prepareQuery(params)
         return {
           url: `products?${_query.toString()}`
         }
@@ -85,6 +107,7 @@ export const backendApi = createApi({
 export const {
   useGetAllCategoriesQuery,
   useGetAllSubCategoriesQuery,
+  useGetBannersQuery,
   useGetProductsQuery,
   useGetProductQuery
 } = backendApi
