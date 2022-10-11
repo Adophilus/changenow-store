aa03739ee477f8e80f0d2ca7ca788c81ad6491da
Adophilus <uchenna19of@gmail.com>
2022-10-11 15:33:47 +0100

    added product query to cart view

--- src/views/Cart.tsx
+++ src/views/Cart.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
-import db from '../Database'
 import '../assets/Loader.scss'
 import '../assets/Product.scss'
 import '../assets/Cart.scss'
@@ -8,21 +7,13 @@ import Layout from '../components/layout/Layout'
 import { IProduct } from '../types/Collections'
 import { useAppSelector } from '../hooks/Store'
 import { ICartState } from '../features/Cart'
-
-enum Errors {
-  CART_EMPTY = 'No item in cart!'
-}
-
-interface IState {
-  error: boolean | string
-  loading: boolean
-  products: IProduct[]
-}
+import { useGetProductsByIdsQuery } from '../services/Backend'
 
 const ProductRow: React.FC<{ product: IProduct; cart: ICartState }> = ({
   product,
   cart
 }) => {
+  console.log(product)
   return (
     <div className="row product-row">
       <div className="col-lg-3">
@@ -49,7 +40,7 @@ const ProductRow: React.FC<{ product: IProduct; cart: ICartState }> = ({
       <div className="col-lg-3">- 1 +</div>
       <div className="col-lg-3">
         <strong>
-          {(product.price as number) * (cart.items.get(product.id) ?? 0)}
+          {(product.price as number) * (cart.items[product.id] ?? 0)}
         </strong>
       </div>
     </div>
@@ -57,52 +48,21 @@ const ProductRow: React.FC<{ product: IProduct; cart: ICartState }> = ({
 }
 
 const Cart: React.FC = () => {
-  const { cart } = useAppSelector((state) => state)
-  const [state, setState] = useState<IState>({
-    error: false,
-    loading: true,
-    products: []
-  })
-
-  useEffect(() => {
-    ;(async () => {
-      if (Object.keys(cart.items).length === 0) {
-        return setState({ ...state, loading: false, error: Errors.CART_EMPTY })
-      }
-
-      const filter = Object.keys(cart.items)
-        .map((item) => `id = '${item}'`)
-        .join('||')
-
-      const productMeta = await db.getProducts({
-        options: { filter, $autoCancel: false }
-      })
-      return setState({
-        loading: false,
-        error: false,
-        products: productMeta.items
-      })
-    })().catch((err) =>
-      setState({ ...state, loading: false, error: err.message })
-    )
-  }, [])
+  const cart = useAppSelector((state) => state.cart)
+  const {
+    error,
+    isLoading,
+    data: products
+  } = useGetProductsByIdsQuery(Object.keys(cart.items))
 
   return (
     <Layout>
-      {state.loading ? (
+      {isLoading ? (
         <div className="loader-full-screen" aria-busy="true"></div>
-      ) : state.error === Errors.CART_EMPTY ? (
-        <div className="loader-full-screen no-products-found">
-          <i className="icon bi bi-bag"></i> <h3>{state.error}</h3>
-          <Link to="/store">
-            <i className="bi bi-chevron-left"></i>
-            Back to store
-          </Link>
-        </div>
-      ) : state.error !== false ? (
+      ) : error != null ? (
         <div className="loader-full-screen no-products-found">
           <i className="bi bi-emoji-frown"></i>
-          <h3>{state.error}</h3>
+          <h3>An error occurred!</h3>
         </div>
       ) : (
         <article className="cart">
@@ -114,7 +74,7 @@ const Cart: React.FC = () => {
               <div className="col-lg-3">TOTAL</div>
             </div>
 
-            {state.products.map((product) => (
+            {products.map((product) => (
               <ProductRow key={product.id} product={product} cart={cart} />
             ))}
           </div>
