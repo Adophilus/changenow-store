da0a3345ab51afa7fe62ceb7c0901629821c6c13
Adophilus <uchenna19of@gmail.com>
2022-11-06 21:29:26 +0100

    added config file

---
+++ utils/Config.ts
@@ -0,0 +1,16 @@
+import { getEnv } from './utils/Env'
+
+const ENV = getEnv({ env : { dir: '..'}})
+
+export default {
+  db: {
+    url: ENV.DB_URL
+  },
+  pocketbase: {
+    url: ENV.POCKETBASE_URL,
+    admin: {
+      email: ENV.POCKETBASE_ADMIN_EMAIL,
+      password: ENV.POCKETBASE_ADMIN_PASSWORD
+    }
+  }
+}
