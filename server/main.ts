e8c81fcccdb962a78be84b8f87c30c16f434dd55
Adophilus <uchenna19of@gmail.com>
2022-11-12 18:07:05 +0100

    changed config

--- server/main.ts
+++ server/main.ts
@@ -46,7 +46,7 @@ export default class AppServer extends Server {
     this.app.get('*', (req, res) => {
       let returnFile: string
       if (req.originalUrl.startsWith("/assets/"))
-        returnFile = req.originalUrl.split('?').shift()
+        returnFile = String(req.originalUrl.split('?').shift())
       else
         returnFile = '/index.html'
       return res.sendFile(returnFile, { root: config.project.frontendDir })
